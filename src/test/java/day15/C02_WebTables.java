6b7c277d8b218ab06c19c18081f7f627db3a3a24
mrasit94 <rsdmuhammet@gmail.com>
2023-02-12 06:42:18 -0500

    12/02/2023

--- src/test/java/day15/C02_WebTables.java
+++ src/test/java/day15/C02_WebTables.java
@@ -53,4 +53,18 @@ public class C02_WebTables extends TestBase {
         sutun5.forEach(veri-> System.out.println(veri.getText()));
     }
 
+//    Task 5 : Iki parametreli bir Java metot oluşturalım: printData
+//    Parameter 1 = row numarasi
+//    Parameter 2 = column numarasi
+//    printData(2,3);  => 2nd satir, 3rd sutun daki datayı print etsin
+    public void printData(int satir, int sutun){
+        driver.get("https://the-internet.herokuapp.com/tables");
+        String myXpath = "//table[@id='table1']//tbody//tr["+satir+"]//td["+sutun+"]";
+        System.out.println(driver.findElement(By.xpath(myXpath)).getText());
+    }
+    @Test
+    public void printDataTest(){
+        printData(2,3);
+    }
+
 }
