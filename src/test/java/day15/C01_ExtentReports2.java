e841a732d1ec8ac0fea45f94ccac5deef1a90759
mrasit94 <rsdmuhammet@gmail.com>
2023-02-12 04:20:41 -0500

    12/02/2023

--- src/test/java/day15/C01_ExtentReports2.java
+++ src/test/java/day15/C01_ExtentReports2.java
@@ -1,23 +1,24 @@
-package day14;
+package day15;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import utilities.TestBase;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class C03_ExtentReports extends TestBase {
+public class C01_ExtentReports2 extends TestBase {
 
     //    HATIRLAMAMIZ GEREKEN 3 CLASS
     protected static ExtentReports extentReports;
     protected static ExtentHtmlReporter extentHtmlReporter;
     protected static ExtentTest extentTest;
-    @Test
-    public void extentReportsTest(){
+    @BeforeClass
+    public static void extentReportsSetUp(){
 //      REPORT  PATH
         String currentTime = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         String path = System.getProperty("user.dir")+"/test-output/reports/"+currentTime+"html_report.html";
@@ -39,19 +40,20 @@ public class C03_ExtentReports extends TestBase {
 //        ********************************RAPOR AYARLARI BITTI****************************
 //        Raporu projeme ekliyorum
         extentReports.attachReporter(extentHtmlReporter);
-
 //        Extent Test objesi ni olustur
         extentTest = extentReports.createTest("Extent Report Login Test","Smoke Test Raporu");
-//        TUM AYARLAR BITTI. EXTENT TEST OBJESI ILE LOGLAMA(RAPORA YAZDIRMA) ISLEMINI YAPABILIRIM
-        extentTest.pass("Kullanici ana sayfaya gider");
-        driver.get("https://www.techproeducation.com");
-//        LMS SAYFASINA GIDELIM
-        extentTest.pass("Kullanici LMS sayfasina gider");
-        driver.findElement(By.linkText("LMS LOGIN")).click();
-//        TEST BITTI
-        extentTest.pass("TEST BASARIYLA GERCEKLESDI");
-//        RAPORU GOSTER. RAPORUN OLUSMASI ICIN BU ADIM ZORUNLUDUR
+    }
+    @Test
+    public void extentReportsTest(){
+        extentTest.pass("PASS");
+        extentTest.info("BILGILENDIRME NOTU");
+        extentTest.fail("FAILED");
+        extentTest.warning("UYARI MESAJI");
+        extentTest.skip("ATLAMA MESAJI");
+        extentTest.fatal("COKUS HATASI");
+    }
+    @AfterClass
+    public static void extentReportsTearDown() {
         extentReports.flush();
     }
-
 }
